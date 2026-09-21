864a041f2d447393f241a30310852c17d740135e
Rowaida <200054008@must.edu.eg>
2026-09-21 22:13:33 +0300

    Login is added

---
+++ src/app/login/login.ts
@@ -0,0 +1,10 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {}
