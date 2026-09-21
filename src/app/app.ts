1fa6ba1706ddb135d7d289dc1fd898bc2325d590
Rowaida <200054008@must.edu.eg>
2026-09-22 02:43:41 +0300

    Register is Preparing...

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './login/login';
+import { Register } from './register/register';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet, Register],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Diva');
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,10 +1,31 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { validate } from '@angular/forms/signals';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login',
-  imports: [],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [ReactiveFormsModule , RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  
+  showPassword : boolean = false;
+  loginForm = new FormGroup({
+    userEmail: new FormControl(null, [Validators.required, Validators.email]),
+    userPassword: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),
+    ]),
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
+  displayPassword(){
+    this.showPassword = !this.showPassword;
+  }
+}
